8f202d57fa3cb5c2bc396ae00d9b889474e09a56
beyza <beyzayigit017@gmail.com>
2023-01-09 21:08:42 +0300

    .

--- aResimAcma.py
+++ aResimAcma.py
@@ -4,16 +4,16 @@ import numpy as np
 img=cv2.imread("630e71c498e60.jpg")
 imgGri=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("totoro", img)
-cv2.imshow("gri totoro", imgGri)
+cv2.imshow("TOTORO", img)
+cv2.imshow("GRİ TOTORO", imgGri)
 
-size_y = img.shape[0]  #genişlik
-size_x = img.shape[1]  #yükseklik
+sizeY = img.shape[0]  #genişlik
+sizeX = img.shape[1]  #yükseklik
 kanal = img.shape[2]  #renkli resimlerde kullanılır
 
-print("yükseklik:", size_y)
-print("genişlik:", size_x)
-print("kanal sayısı:", kanal)
+print("yukseklik:", sizeY)
+print("genislik:", sizeX)
+print("kanal sayisi:", kanal)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
