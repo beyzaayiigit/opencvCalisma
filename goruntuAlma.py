c904836dad12a5403ad01b758811ef8f4429a172
beyza <beyzayigit017@gmail.com>
2023-01-09 20:22:13 +0300

    .

--- goruntuAlma.py
+++ goruntuAlma.py
@@ -7,9 +7,9 @@ while 1:
     ret, frame = cap.read()
     
     frame = cv2.flip(frame, 1)
-    cv2.imshow("webcam", frame)
+    cv2.imshow("WEBCAM", frame)
     
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    if cv2.waitKey(1) & 0xFF == ord("b"):
         break
     
 cap.release()
